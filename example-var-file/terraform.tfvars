d92bf1df940e0e00cab3ed533c520d7d679bd4b2
Bartosz Chmielewski <bartoszch@gmail.com>
2023-12-22 11:08:51 +0100

    initial commit

---
+++ example-var-file/terraform.tfvars
@@ -0,0 +1,6 @@
+APIID=""
+APIKEY=""
+WebsiteFQDN="terraform.spm.pl"
+WebsiteIP="20.215.145.47"
+OriginPOP="war"
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "APIID" {
+  type        = string
+  description = "API Key ID"
+}
+
+variable "APIKEY" {
+  type        = string
+  description = "API Key"
+}
+
